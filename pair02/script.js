79021d087ccf140760e2d35a2941ed2a881b701b
Pugach Vsevolod <l901752@gmail.com>
2026-09-09 10:14:32 +0300

    Add files via upload

---
+++ pair02/script.js
@@ -0,0 +1,86 @@
+// let temp = Number(prompt('Enter a temperature'));
+// let result;
+// if (temp < 0) {
+//     result = 'cold'
+// }
+// else if (temp >= 25) {
+//     result = 'hot'
+// }
+// else if (temp > 0 && temp < 24) {
+//     result = 'normal'
+// }
+// alert(result);
+
+// let number = Number(prompt('Enter a number'));
+// let result;
+// if (number % 2 === 0){
+//     result = 'even'
+// }
+// else if (number % 2 > 0){
+//     result = 'odd'
+// }
+//
+// alert(result);
+
+// const login = "admin";
+// const password = 1234;
+//
+// let user_login = prompt("Enter your username");
+// let user_password = prompt("Enter your password");
+// if (user_login === login && user_password === password) {
+//     alert("Access granted")
+// }
+// else {
+//     alert("Access denied")
+// }
+
+// post - 100
+// courier - 200
+// pickup - 0
+// let deliveryType = prompt('What is your delivery type?');
+// let cost = 0;
+//
+// switch (deliveryType) {
+//     case 'post':
+//         cost = 100;
+//         break;
+//     case 'courier':
+//         cost = 200;
+//         break;
+//     case 'pickup':
+//         cost = 0;
+//         break;
+//     default:
+//         cost = "no answer"
+// }
+// alert(cost);
+
+// let weekday = Number(prompt('What is your number '));
+// let result;
+// switch (weekday) {
+//     case 1:
+//         result = 'Monday';
+//         break;
+//     case 2:
+//         cost = 2;
+//         result = 'Tuesday';
+//         break;
+//     case 3:
+//         result = 'Wednesday';
+//         break;
+//     case 4:
+//         result = 'Thursday';
+//         break;
+//     case 5:
+//         result = 'Friday';
+//         break;
+//     case 6:
+//         result = 'Saturday';
+//         break;
+//     case 7:
+//         result = 'Sunday';
+//         break;
+//     default:
+//         result = "No answer"
+// }
+// alert(result);
